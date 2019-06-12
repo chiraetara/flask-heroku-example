1b3b6fe53e94e2f9dad2c7570968c59e57250507
chiraetara <51397056+chiraetara@users.noreply.github.com>
2019-06-12 13:47:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 @app.route('/')
 def index():
-return "Hello World สวัสดีชาวโลก"
+   return "Hello World สวัสดีชาวโลก"
 
 @app.route("/callback", methods=['POST'])
 def callback():
